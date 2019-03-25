8fc19d1f1cde052ba2e1d5a5d97827d41d7a662d
rKaur <ravneet.kaur@nus.edu.sg>
2019-03-25 21:49:24 +0800

    modified file

--- Task 01/helloWorldModCreate.c
+++ Task 01/helloWorldModCreate.c
@@ -4,13 +4,16 @@
 
 MODULE_LICENSE("GPL");
 
+static char *who = "ravneet";
+module_param(who, charp,S_IRUGO);
+
 static int hello_init(void){
- printk(KERN_ALERT "Hello World \n");
+ printk(KERN_ALERT "Hello World! I go by the name of %s\n", who);
  return 0;
 }
 
 static void hello_exit(void){
- printk(KERN_ALERT "Bye World \n");
+ printk(KERN_ALERT "Bye World! Until next time %s\n", who);
 }
 
 module_init(hello_init);
